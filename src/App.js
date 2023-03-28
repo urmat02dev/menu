a6b94c60d466265aa509a5f907fe51a9cc296468
Urmat <urmatzhekshenov@gmail.com>
2023-03-28 16:01:43 +0600

    First

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import {Route, Routes} from "react-router-dom";
 import Starts from "./components/starts/Starts";
+import Header from "./components/header/Header";
 
 function App() {
   return (
    <>
+
      <Routes>
        <Route path="/" element={<Starts/>}/>
      </Routes>
