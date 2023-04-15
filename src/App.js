4566ebcf4fc64ca06180b893f01087041b2142d8
NOTNIK_KG <nasirovasylai@gmail.com>
2023-04-15 11:04:41 +0600

    add

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import {Route, Routes} from "react-router-dom";
 import Starts from "./components/starts/Starts";
 import MainPage from "./components/main-page/MainPage";
 import Basket from "./components/basket/Basket";
+import Modal from "./components/modal/Modal";
+import React, {useState} from "react";
 
 function App() {
+const [modalActive,setActive] = useState(false)
+
   return (
    <>
+       <button  onClick={() => setActive(true)}>MODAL</button>
 
      <Routes>
        <Route path="/" element={<Starts/>}/>
        <Route path="/main" element={<MainPage/>}/>
        <Route path="/basket" element={<Basket/>}/>
-     </Routes>
+    </Routes>
+       <Modal active={modalActive} setActive={setActive}/>
    </>
   );
 }
--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import "./Modal.scss"
-const Modal = () => {
-  return (
-    <div>
+import {BsBasket} from "react-icons/bs";
+import meal from "../../assets/img/iPhone 13/image 15.png"
 
-    </div>
-  );
+const Modal = ({active, setActive}) => {
+    return (
+        <div id="modal">
+            <div className="container">
+                <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
+                    <div className="modal--content" onClick={e => e.stopPropagation()}>
+                        <button className="modal--content__none" onClick={() => setActive(false)}>&#x2715;</button>
+                        <img className="modal--content__img1" src={meal} alt=""/>
+                        <div className="modal--content__card" >
+                            <h1>Гранола</h1>
+                            <p>Гранола Карамелизированная хрустящая смесь полезных злаков, греческий йогурт, свежие
+                                фрукты и
+                                ягоды</p>
+                            <i>250г</i>
+                            <div className="modal--content__card--price">
+                                <h4>Цена:</h4>
+                                <i>238c</i>
+                            </div>
+                            <div className="modal--content__card--btn">
+                                <button className="modal--content__card--btn__addBtn">Добавить в<BsBasket
+                                    className="modal--content__card--btn__addBtn--icon"/></button>
+                                <button className="modal--content__card--btn__plusBtn">-  1  +</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
 };
 
 export default Modal;
