18cf1c73bd3ab5ec84854f0a534617983b4560f0
cerbent <kalykovadilet2004@gmail.com>
2023-07-05 14:15:38 +0600

    finishedTabs

--- src/components/main-page/foods/Foods.js
+++ src/components/main-page/foods/Foods.js
@@ -1,52 +1,56 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import "./Foods.scss"
 import {useTranslation} from "react-i18next";
 import FoodCard from "./FoodCard";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import Slider from "react-slick"
+import {BURGER_ACTIVE, BURGER_MENU} from "../../../redux/Reducer/ActionTypes";
 
 
 const Foods = () => {
   const {t} = useTranslation()
+  const dispatch = useDispatch()
   const {foods,burgerMenu,active} = useSelector(state => state)
+  const [tabsInMeal, setTabsInMeal] = useState(0)
   const getTitle = () => {
   if (active === 1){
-    return t('category.foods.one')
+    return t('category.foods.main')
 }
   else if (active === 2){
-    return t('category.foods.two')
+    return t('category.foods.main')
   }
   else if (active === 3){
-    return t('category.foods.three')
+    return t('category.foods.main')
   }
   else if (active === 4){
-    return t('category.foods.four')
+    return t('category.foods.main')
   }
   else if (active === 5){
-    return t('category.foods.five')
+    return t('category.foods.main')
   }
   else if (active === 6){
-    return t('category.foods.six')
+    return t('category.foods.main')
   }
   else if (active === 7){
-    return t('category.foods.seven')
+    return t('category.foods.main')
   }
   else if (active === 8){
-    return t('category.foods.eight')
+    return t('category.foods.main')
   }
   else if (active === 9){
-    return t('category.salads.one')
+    return t('category.salads.main')
   }
   else if (active === 10){
-    return t('category.salads.two')
+    return t('category.salads.main')
   }
   else if (active === 11){
-    return t('category.salads.three')
+    return t('category.salads.main')
   }
   else if (active === 12){
-    return t('category.drinks.one')
+    return t('category.drinks.main')
   }
   else if (active === 13){
-    return t('category.drinks.two')
+    return t('category.drinks.main')
   }
   else if (active === 14){
     return t('category.5')
@@ -75,7 +79,10 @@ const Foods = () => {
     }
     else window.scrollTo({top:0, behavior:"smooth"})
   }
+
+
   const scroolRef = useRef(null)
+  const meals = foods.filter(el => el.category_name === "Первые блюда" || el.category_name === "Вторые блюда из говядины" || el.category_name === "Вторые блюда из рыбы" || el.category_name === "Вторые блюда из баранины" || el.category_name === "Общие блюда" || el.category_name === "Вторые блюда из курицы" || el.category_name === "Банкетные блюда" )
   const rolls = foods.filter(el => el.category_name === "Роллы")
   const coldSnacks = foods.filter(el => el.category_name === "Холодные закуски")
   const saladsMayonnaise = foods.filter(el => el.category_name === "Салаты с майонезом")
@@ -88,26 +95,95 @@ const Foods = () => {
   const secondCoursesOfLamb = foods.filter(el => el.category_name === "Вторые блюда из баранины")
   const generalDishes = foods.filter(el => el.category_name === "Общие блюда")
   const banquetDishes = foods.filter(el => el.category_name === "Банкетные блюда")
-  const lambDishes = foods.filter(el => el.category_name === "Блюда из баранины на заказ")
   const kebabs = foods.filter(el => el.category_name === "Шашлыки")
   const childrensMenu = foods.filter(el => el.category_name === "Детское меню")
   const sideDishes = foods.filter(el => el.category_name === "Гарниры")
   const hotDrinks = foods.filter(el => el.category_name === "Горячие напитки")
   const coldDrinks = foods.filter(el => el.category_name === "Холдные")
   const flour = foods.filter(el => el.category_name === "Мучные")
+      const settings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              initialSlide: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      };
 
   useEffect(() => {
     WindowClick()
   },[active])
-  console.log("secondCoursesOfBeef",secondCoursesOfBeef)
+  console.log("set",tabsInMeal)
 
   return (
     <div id="food">
       <div className="container">
         <div className="food">
-          <h1>{getTitle()}</h1>
+          <h1 onClick={() => setTabsInMeal(0)}>{getTitle()}</h1>
+          <div>
+            <Slider {...settings}>
+              <div>
+                <div className={tabsInMeal === 1  ? "active-tabsMeal" : "tabsMeal"} onClick={() => setTabsInMeal(1) }>{t('category.foods.one')}</div>
+              </div>
+              <div>
+                <div className={tabsInMeal === 2 ? "active-tabsMeal" : "tabsMeal"} onClick={() => setTabsInMeal(2)}>{t('category.foods.two')}</div>
+              </div>
+              <div>
+                <div className={tabsInMeal === 3  ? "active-tabsMeal" : "tabsMeal"} onClick={() => setTabsInMeal(3)}>{t('category.foods.three')}</div>
+              </div>
+              <div>
+                <div className={tabsInMeal === 4  ? "active-tabsMeal" : "tabsMeal"} onClick={() => setTabsInMeal(4)}>{t('category.foods.four')}</div>
+              </div>
+              <div>
+                <div className={tabsInMeal === 5  ? "active-tabsMeal" : "tabsMeal"} onClick={() => setTabsInMeal(5)}>{t('category.foods.five')}</div>
+              </div>
+              <div>
+                <div className={tabsInMeal === 6  ? "active-tabsMeal" : "tabsMeal"} onClick={() => setTabsInMeal(6)}>{t('category.foods.six')}</div>
+              </div>
+              <div>
+                <div className={tabsInMeal === 7  ? "active-tabsMeal" : "tabsMeal"} onClick={() => setTabsInMeal(7)}>{t('category.foods.seven')}</div>
+              </div>
+              <div>
+                <div className={tabsInMeal === 8  ? "active-tabsMeal" : "tabsMeal"} onClick={() => setTabsInMeal(8)}>{t('category.foods.eight')}</div>
+              </div>
+            </Slider>
+          </div>
+          {
+              (active === 1 && tabsInMeal === 0)  &&
+              meals.map(el =>{
+                return <FoodCard el={el}
+                                 key={el.id}
+                />
+              })
+          }
             {
-              active === 1  &&
+              tabsInMeal === 1  &&
                   firstMeal.map(el =>{
                 return <FoodCard el={el}
                                  key={el.id}
@@ -115,7 +191,7 @@ const Foods = () => {
               })
             }
           {
-            active === 2 ?
+            tabsInMeal === 2 ?
                 generalDishes.map(el =>{
                   return <FoodCard el={el}
                                    key={el.id}
@@ -124,7 +200,7 @@ const Foods = () => {
                 : false
           }
           {
-             active === 3 ?
+             tabsInMeal === 3 ?
                 secondCoursesOfChicken.map(el =>{
                   return <FoodCard el={el}
                                    key={el.id}
@@ -133,7 +209,7 @@ const Foods = () => {
                 : false
           }
           {
-            active === 4 ? WindowClick &&
+            tabsInMeal === 4 ? WindowClick &&
                 secondCoursesOfBeef.map(el =>{
                   return <FoodCard el={el}
                                    key={el.id}
@@ -142,7 +218,7 @@ const Foods = () => {
                 : false
           }
           {
-            (!burgerMenu && active === 5) ? WindowClick &&
+            tabsInMeal === 5 ? WindowClick &&
                 secondCoursesOfLamb.map(el =>{
                   return <FoodCard el={el}
                                    key={el.id}
@@ -151,7 +227,7 @@ const Foods = () => {
                 : false
           }
           {
-            (!burgerMenu && active === 6) ? WindowClick &&
+            tabsInMeal === 6 ? WindowClick &&
                 secondCoursesOfFish.map(el =>{
                   return <FoodCard el={el}
                                    key={el.id}
@@ -160,7 +236,7 @@ const Foods = () => {
                 : false
           }
           {
-            (!burgerMenu && active === 7) ? WindowClick &&
+            tabsInMeal === 7 ? WindowClick &&
                 banquetDishes.map(el =>{
                   return <FoodCard el={el}
                                    key={el.id}
@@ -169,8 +245,8 @@ const Foods = () => {
                 : false
           }
           {
-            (!burgerMenu && active === 8) ? WindowClick &&
-                lambDishes.map(el =>{
+            tabsInMeal === 8 ? WindowClick &&
+                sideDishes.map(el =>{
                   return <FoodCard el={el}
                                    key={el.id}
                   />
